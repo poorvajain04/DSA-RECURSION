6d7cf9e7ea4c31d89cdd88dab9aba5686182f8ff
Poorva Jain <poorvajain55@gmail.com>
2026-02-09 22:38:37 +0530

    Add files via upload

--- sum_n_to_1.java
+++ sum_n_to_1.java
@@ -1,2 +1,12 @@
 public class sum_n_to_1 {
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(sum(n));
+    }
+    public static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
 }
