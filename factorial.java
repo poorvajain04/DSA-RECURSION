9a328cbde7af3915ccdce32091f14907e1c9417d
Poorva Jain <poorvajain55@gmail.com>
2026-02-09 22:34:22 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,12 @@
+public class factorial {
+    public static void main(String[] args) {
+        int n=20;
+        System.out.println(fact(n));
+    }
+    public static long fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+}
